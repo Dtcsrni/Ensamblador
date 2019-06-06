248cc5659360026c50dd4c7ab83d9f4fecd4b6e0
Erick Vega <evega-12@hotmail.com>
2019-06-05 21:46:12 -0500

    Create incrementoyDecrementoBanderas.asm

---
+++ incrementoyDecrementoBanderas.asm
@@ -0,0 +1,35 @@
+COMMENT °
+
+| Programa para demostrar la bandera de acarreo y sus cambios utilizando incremento y decremento
+| Erick Renato Vega Cerón
+| Ingeniería en Sistemas Computacionales
+
+°
+
+include irvine32.inc
+
+.code
+main proc
+
+		mov eax,0FFFFFFFFH	;se llena el registro con 32 bits de datos
+		call dumpregs	;se muestran en pantalla las banderas
+		inc eax	;se incrementa el registro eax una vez
+		inc eax	;se incrementa el registro eax dos veces
+		
+		call dumpregs	;se muestran en pantalla las banderas
+
+		mov eax,0
+
+		dec eax ;se decrementa el registro eax una vez
+		dec eax	;se decrementa el registro eax dos veces
+
+		call dumpregs	;se muestran en pantalla las banderas
+
+	call writeInt	;se manda el resultado del registro eax a pantalla
+
+	invoke ExitProcess,0
+main endp
+end main
+	invoke ExitProcess,0
+main endp
+end main
