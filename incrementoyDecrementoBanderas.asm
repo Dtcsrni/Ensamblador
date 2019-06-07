834dcc40d5d41db2dd289619f92b8c2b0f540b2a
Erick Vega <evega-12@hotmail.com>
2019-06-07 08:00:15 -0500

    corregido

--- incrementoyDecrementoBanderas.asm
+++ incrementoyDecrementoBanderas.asm
@@ -14,15 +14,13 @@ main proc
 		mov eax,0FFFFFFFFH	;se llena el registro con 32 bits de datos
 		call dumpregs	;se muestran en pantalla las banderas
 		inc eax	;se incrementa el registro eax una vez
-		inc eax	;se incrementa el registro eax dos veces
-		
+				
 		call dumpregs	;se muestran en pantalla las banderas
 
 		mov eax,0
 
 		dec eax ;se decrementa el registro eax una vez
-		dec eax	;se decrementa el registro eax dos veces
-
+		
 		call dumpregs	;se muestran en pantalla las banderas
 
 	call writeInt	;se manda el resultado del registro eax a pantalla
