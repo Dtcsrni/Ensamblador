c4116ea9c56b7068f8a43992ed9d457eb74401a1
Erick Vega <evega-12@hotmail.com>
2019-06-16 18:40:47 -0500

    enunciado inicial

---
+++ cronometro.asm
@@ -0,0 +1,5 @@
+Realizar un programa en ensamblador que muestre sus datos al principio (Nombre y materia) y 
+pida al usuario una respuesta con respecto al mensaje "Acceder al cronómetro",
+si la respuesta es "No" salir del programa mostrando "Hasta luego" y si la respuesta es 
+"si" pedir el tiempo (t) del cronometro y llamaruna subrutina que realice el conteo de,
+tiempo para después mostrar el mensaje "(t) seg transcurridos"
