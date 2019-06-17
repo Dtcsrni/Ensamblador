b473e3b4358044aeac70e64e970a86b92642a1c8
Dtcsrni <evega-12@hotmail.com>
2019-06-17 00:40:26 -0500

    mejor comentado y mejorado

--- cronometro.asm
+++ cronometro.asm
@@ -22,6 +22,7 @@ include irvine32.inc
 	numerotiempo DWORD ?
 	transcurrido DWORD ?
 	numero DWORD ?
+	espacio BYTE " ",0
 .code
 main proc
 	;escribir nombre
@@ -54,32 +55,39 @@ cronometro:
 	   jmp fin
 
 salir:																																																																						
-	    mov  edx,OFFSET salida	;se mueve al registro edx el contenido del segundo mensaje
+	    mov  edx, OFFSET salida	;se mueve al registro edx el contenido del segundo mensaje
 		call WriteString	;se llama la instruccion escribir string con el contenido de edx
 fin:	
 invoke ExitProcess,0
 main endp
 
 
-cronometroSub proc
+cronometroSub proc	;la subrutina del cronometro
+ call Clrscr	;se limpia la pantalla
 	   mov  edx,OFFSET tiempo	;se mueve al registro edx el contenido del segundo mensaje
        call WriteString	;se llama la instruccion escribir string con el contenido de edx
-	   call ReadInt
-	   mov numerotiempo,eax
+	   call ReadInt	;leer un entero referente a cuantos segundos se esperará
+	   mov numerotiempo,eax	;mover el valor del entero a la variable numero tiempo
 
-	   mov ecx,numerotiempo
+	   mov ecx,numerotiempo	;mover el tiempo indicado a esperar, al contador
 
 
-	   mov eax,numerotiempo
-	   mov numero,eax
+	   mov eax,numerotiempo	;mover también el tiempo indicado al registro eax
+	   mov numero,eax	;mover el contenido de eax a la variable numero
 
-	   contador:
-	   mov eax,ecx
-	   sub eax,numero
-	   neg eax
-	   add eax,1
-	   call WriteDec	   
+	   contador:	;inicio del bucle para contar el tiempo
+	   mov eax,ecx	;mover el valor actual del contador al registro eax 
+	   sub eax,numero	;restarle al registro eax el valor de la variable numero, que es el tiempo inicial indicado
+	   neg eax	;convertir el registro eax a su complemento a dos para eliminar el signo
+	   add eax,1	;sumar uno para que empiece desde un segundo
 
+	   mov  edx,OFFSET espacio	;se mueve al registro edx el contenido del segundo mensaje
+       call WriteString	;se llama la instruccion escribir string con el contenido de edx
+
+	   call WriteDec	;mostrar el valor del tiempo en pantalla
+
+	   mov  edx,OFFSET espacio	;se mueve al registro edx el contenido del segundo mensaje
+       call WriteString	;se llama la instruccion escribir string con el contenido de edx
 	   
 
 	   mov  edx,OFFSET segundos	;se mueve al registro edx el contenido del segundo mensaje
@@ -87,14 +95,14 @@ cronometroSub proc
 	   
 	   
 	   mov  eax,1000 ; esperar un segundo
-       call Delay
+       call Delay	;se procede a la espera
 	   call Crlf	;se salta la linea
-	   dec ecx
-	   jnz contador
+	   dec ecx	;se decrementa el contador
+	   jnz contador	;regresa si el contador no vale cero aún
 	   
 	   
 
-ret
-cronometroSub endp
+ret	;retorno (equivalente a return=0)
+cronometroSub endp	
 
 end main
