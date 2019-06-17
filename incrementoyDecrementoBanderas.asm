5f924236a7c66bfd74bd55ea3507aa6583158e92
Erick Vega <evega-12@hotmail.com>
2019-06-17 08:35:02 -0500

    corregido, rollback a primer version

--- incrementoyDecrementoBanderas.asm
+++ incrementoyDecrementoBanderas.asm
@@ -1,6 +1,6 @@
 COMMENT °
 
-| Programa para demostrar la bandera de acarreo y sus cambios utilizando suma y resta
+| Programa para demostrar la bandera de signo y sus cambios utilizando incremento y decremento
 | Erick Renato Vega Cerón
 | Ingeniería en Sistemas Computacionales
 
@@ -11,24 +11,21 @@ include irvine32.inc
 .code
 main proc
 
-
 		mov eax,0FFFFFFFFH	;se llena el registro con 32 bits de datos
-		call dumpregs	;se muestran en pantalla las banderas previo a saturar el registro
-
-		add eax,1 ;se suma 1 al registro eax para saturar el registro y activar la bandera CF (acarreo)
-
 		call dumpregs	;se muestran en pantalla las banderas
+		inc eax	;se incrementa el registro eax una vez
+		call dumpregs	;se muestran en pantalla las banderas
+		inc eax	;se incrementa el registro eax dos veces
 		
-		sub eax,1 ;se resta 1 al registro eax para limpiar la bandera de acarreo
+		
+
+		mov eax,0 ;se limpia el registro
+
+		dec eax ;se decrementa el registro eax una vez
+		dec eax	;se decrementa el registro eax dos veces
 
 		call dumpregs	;se muestran en pantalla las banderas
 
-		sub eax,1 ;se resta 1 al registro eax para limpiar la bandera de acarreo
-
-		call dumpregs	;se muestran en pantalla las banderas
-
-
-	
 	call writeInt	;se manda el resultado del registro eax a pantalla
 
 	invoke ExitProcess,0
