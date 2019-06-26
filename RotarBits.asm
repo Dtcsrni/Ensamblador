c358d44a9dab2698779c82d01f5470af0d2961a0
Erick Vega <evega-12@hotmail.com>
2019-06-25 20:52:27 -0500

    Update RotarBits.asm

--- RotarBits.asm
+++ RotarBits.asm
@@ -1,24 +1,23 @@
-; PIC16F887 Configuration Bit Settings
+
+; PIC16F84A Configuration Bit Settings
 
 ; Assembly source line config statements
 
-#include "p16f887.inc"
+#include "p16f84a.inc"
 
-; CONFIG1
-; __config 0xFFD5
- __CONFIG _CONFIG1, _FOSC_INTRC_CLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_ON & _FCMEN_ON & _LVP_ON
-; CONFIG2
-; __config 0xFFFF
- __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+; CONFIG
+; __config 0xFFF9
+ __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _CP_OFF
 
 
-    LIST p=16f887
-    numero EQU 0
-    numero2 EQU 1
+
+    LIST p=16f84A
+
     ORG 0
 		    ;inicio de instrucciones 
     
-    bsf STATUS,RP0
+
+    BSF STATUS, RP0
     
     bsf TRISA,0
     bsf TRISA,1
@@ -31,23 +30,25 @@
     bcf TRISB,2
     bcf TRISB,3
     bcf TRISB,4
+    bcf TRISB,5
+    bcf TRISB,6
+    bcf TRISB,7
     
     bcf STATUS,RP0
     
+   
     
     
 inicio 
     MOVLW PORTA
-    goto rotar
-    
-    
+    goto rotar   
   
-    goto inicio
+   
 rotar
     RLF	W,0
     MOVWF PORTB
+    goto inicio
 
 END    
     
-
 
